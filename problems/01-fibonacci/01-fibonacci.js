8d3fcbde00b99c876767b4f7178505717388bf15
Shreyas-Kathavate <49164617+Shreyas-Kathavate@users.noreply.github.com>
2019-04-05 21:04:46 -0400

    01-fibonacci.js

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,17 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
+    let temp, a = 1,
+        b = 0;
 
+    while (n > 0) {
+        temp = a + b;
+        a = b;
+        b = temp;
+        n--;
+    }
+    return b;
 }
+let n = 10;
+console.log(`the nth number in the sequence is 
+        ${fib(n)}`);

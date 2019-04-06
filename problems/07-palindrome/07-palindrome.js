35dd9da6dede12915b5a9414b96c762977abfbfa
Shreyas-Kathavate <49164617+Shreyas-Kathavate@users.noreply.github.com>
2019-04-06 13:53:43 -0400

    07-palindrome.js

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,19 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
-function isPalindrome(someStr) {
-  
+function palindromeLength(str) {
+    let theString = "";
+    for (let i = 0; i < str.length; i++) {
+        theString += str[str.length - 1 - i];
+    }
+    return theString;
 }
+
+function isPalindrome(someStr) {
+    if (someStr === palindromeLength(someStr)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isPalindrome("malayalam"));

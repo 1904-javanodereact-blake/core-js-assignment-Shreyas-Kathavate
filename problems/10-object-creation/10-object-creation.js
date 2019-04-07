6d64be2c14fd3328ced345cbb9bb744fb272c68c
Shreyas-Kathavate <49164617+Shreyas-Kathavate@users.noreply.github.com>
2019-04-06 21:16:30 -0400

    10-object-creation.js

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,33 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+//object literal notation:
+let anime = {
+    name: `Goku`,
+    age: 167,
+    skills: `off the charts`
+}
+console.log(anime);
+anime.hair = "super spiky";
+console.log(anime);
+// constructor function:
+function harryPotter(name, skill, age) {
+    this.name = name;
+    this.skill = skill;
+    this.age = age;
+}
+let Weasley = new harryPotter(
+    `Ronald`,
+    `Okay at spells`,
+    23);
+console.log(Weasley);
+// es6 class
+class onePiece {
+    constructor(name, skills, age) {
+        this.name = name;
+        this.skills = skills;
+        this.age = age;
+    }
+}
+let Zoro = new onePiece(`Roronoa`, `Greatest Swordsman`, 22);
+console.log(Zoro);

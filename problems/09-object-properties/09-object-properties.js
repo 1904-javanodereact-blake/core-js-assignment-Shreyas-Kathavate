a8629aad40819780811aa4cc7dd2ae6cb3eb1b86
Shreyas-Kathavate <49164617+Shreyas-Kathavate@users.noreply.github.com>
2019-04-07 20:41:45 -0400

    09-object-properties.js

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,13 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    for (let arg in someObj) {
+        console.log(someObj[arg]);
+    }
 }
+let myObj = {
+    name: `Shreyas`,
+    age: 23,
+    height: 174
+}
+objectProperties(myObj);
